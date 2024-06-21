aed9e0d76fc58f3fb7766a6f44cc133b8be74d35
zenuncrack <zenun786@gmail.com>
2024-06-21 18:02:30 +0600

    MSSQL Database Connected and Signup Work Done

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ALlyHub.Models;
+
 
 namespace ALlyHub.Controllers
 {
     public class HomeController : Controller
     {
+        AllyhubEntities db = new AllyhubEntities();
         public ActionResult Index()
         {
             return View();
@@ -46,5 +49,27 @@ namespace ALlyHub.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult Register(ProgramUser programUser)
+        {
+            if (db.ProgramUsers.Any(x => x.UserEmail == programUser.UserEmail))
+            {
+                ViewBag.Notification = "This Email is already Taken";
+                return View();  
+            }
+            else
+            {   
+                db.ProgramUsers.Add(programUser); 
+                db.SaveChanges();
+
+                Session["IDUsSS"] =programUser.UserId.ToString();
+                Session["UsernameSS"]=programUser.UserName.ToString();
+
+                return RedirectToAction("Index","Home");
+
+            }
+           
+        }
+
     }
 }
